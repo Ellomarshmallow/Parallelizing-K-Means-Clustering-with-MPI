71e229bec8d49325a6b4a6f9cad196af5d028cf7
Eleonora Renz <eleonora.renz@hpc-head-n1.unitn.it>
2023-01-12 12:30:31 +0100

    merge

---
+++ benchmarking/benchmarking.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+import fnmatch
+
+mnt = "/home/eleonora.renz/hpc4ds-project/benchmarking/"
+experiment = ["light/", "heavy/", "cpu-increase"] #TODO: automate for all
+experiment = "light/"
+
+df = []
+
+for filename in os.listdir(mnt+experiment):
+    f = os.path.join(mnt+experiment, filename)
+    if fnmatch.fnmatch(filename, '*sh.o*'):
+      df_part = pd.read_csv(f, sep=',', names=['nnodes', 'ncpus', 'nproc', 'bytes', 'time'])
+      df.append(df_part)
+
+df = pd.concat(df, sort=False)
+
+# Average over several runs
+grouped_df = df.groupby(['nnodes', 'ncpus', 'nproc'])
+df = grouped_df[['time']].mean()
+df.reset_index(inplace = True)
+
+########
+# Plot #
+########
+
+xlabels = ['1', '2', '4', '8', '16', '32', '64', '128']
+plt.title(experiment.replace('/', ''))
+suptitle = "nodes:" + str(df.nnodes.unique()) + ", cpus:" + str(df.ncpus.unique())
+plt.suptitle(suptitle, y=0.05)
+plt.plot(df.nproc, df.time)
+plt.xticks(df.nproc, xlabels)
+plt.savefig(mnt+'plots/'+experiment.replace('/', ''))
+plt.show()
